950db7f9b53b8d39d41ec9582f4ec46a06284030
emresener33 <emre.sener33@gmail.com>
2021-03-24 23:50:59 +0300

    24.03.2021 Commit2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
-import {FormsModule,ReactiveFormsModule} from "@angular/forms"
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,10 +13,11 @@ import { NaviComponent } from './components/navi/navi.component';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
-import{ToastrModule} from "ngx-toastr";
+import { ToastrModule } from "ngx-toastr";
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 
 
@@ -30,8 +31,8 @@ import { LoginComponent } from './components/login/login.component';
     FilterPipePipe,
     CartSummaryComponent,
     ProductAddComponent,
-    LoginComponent,   
-    
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -41,10 +42,12 @@ import { LoginComponent } from './components/login/login.component';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     ToastrModule.forRoot({
-      positionClass:"toast-bottom-right"
+      positionClass: "toast-bottom-right"
     })
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
